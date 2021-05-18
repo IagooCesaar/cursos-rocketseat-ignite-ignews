bdec9a6ae7702de90f4f739dd512a560cd23e4f2
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-18 10:33:57 -0300

    primeiro exemplo getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+import { GetServerSideProps } from 'next'
+
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss'
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props)
+
   return (
     <>
       <Head>
@@ -24,4 +28,12 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      nome: 'Iago'
+    }
+  }
 }
