24773bdaf43747344b1bf08bd9e862f534000587
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-18 15:01:48 -0300

    função signOut do Next Auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,6 +1,6 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
-import { signIn, useSession } from 'next-auth/client'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss';
 
@@ -12,6 +12,7 @@ export function SignInButton() {
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
       {session.user.name}
