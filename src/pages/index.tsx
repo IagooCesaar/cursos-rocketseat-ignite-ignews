a6baa66d6baed8284c39985f70b5946db25479c0
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-18 08:52:01 -0300

    boa prática para semantica do html do componente

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,13 @@ import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
+    <>
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      Hello <span>World</span>
-    </h1>
+      <h1 className={styles.title}>
+        Hello <span>World</span>
+      </h1>
+    </>
   )
 }
