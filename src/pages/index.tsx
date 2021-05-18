e83c398c002b1a998b19664cf6c12c22f0eb9314
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-18 08:51:15 -0300

    boa prática para título dinâmico

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Head from 'next/head';
+
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello <span>World</span></h1>
+    <h1 className={styles.title}>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      Hello <span>World</span>
+    </h1>
   )
 }
