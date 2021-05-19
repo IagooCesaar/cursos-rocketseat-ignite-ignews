de7e52b86d651162bda5d08aec2fd93b6e6748bb
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-19 10:31:58 -0300

    salvando inscrições no FaunaDB

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -1,8 +1,35 @@
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../../services/fauna'
+import { stripe } from '../../../services/stripe';
+
 export async function saveSubscription(
   subscriptionId: string,
   customerId: string
 ) {
-  // Buscar usuário no FaunaDB com stripe_customer_id (user_by_stripe_customer_id)
+  const userRef = await fauna.query(
+    q.Select(
+      "ref",
+      q.Get(
+        q.Match(
+          q.Index('user_by_stripe_customer_id'),
+          customerId
+        ),
+      )
+    )
+  );
 
-  // Salvar dados da subscription do usuário no FaundaDB
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id,    
+  }
+  await fauna.query(
+    q.Create(
+      q.Collection('subscriptions'),
+      { data: subscriptionData }
+    )
+  )
 }
