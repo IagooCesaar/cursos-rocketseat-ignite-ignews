31eb29c800e047b2656749c9d8130b197213b7e2
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-20 14:06:04 -0300

    Componente para identificar Link ativo

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,30 @@
+import { ReactElement, cloneElement } from "react";
+import { useRouter } from 'next/router';
+import Link, { LinkProps } from "next/link";
+
+interface IActiveLinkProps extends LinkProps {
+  children: ReactElement;
+  activeClassName: string;
+}
+
+export function ActiveLink({
+  children,
+  activeClassName,
+  ...rest
+}: IActiveLinkProps) {
+  const { asPath } = useRouter();
+
+  const className = asPath === rest.href
+    ? activeClassName
+    : '';
+
+  return (
+    <Link {...rest} >
+      {
+        cloneElement(children, {
+          className
+        })
+      }
+    </Link>
+  )
+}
