29fa6e2cf5cf5db82bac4f5691aa6b0c6ab781cd
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-20 13:23:52 -0300

    Formatando dados para exibição

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -50,12 +50,20 @@ export const getStaticProps: GetStaticProps = async () => {
     fetch: ['publication.title', 'publication.content'],
     pageSize: 100,
   })
-
-  console.log('Prismic response =>', JSON.stringify(response, null, 2))
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
 
   return {
-    props: {
-
-    }
+    props: { posts }
   }
 }
