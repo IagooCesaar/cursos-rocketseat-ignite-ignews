a052cef1b3ec2b97b243ebc7da170625d3455e2d
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-16 20:55:03 -0300

    criando corpo do teste do componente ActiveLink

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,3 +1,12 @@
-test('ActiveLink render correctly', () => {
+import React from 'react';
+import { render } from '@testing-library/react';
 
+import { ActiveLink } from '.';
+
+test('ActiveLink render correctly', () => {
+  render(
+    <ActiveLink href="/" activeClassName="active">
+      <a>Home</a>
+    </ActiveLink>
+  )
 })
