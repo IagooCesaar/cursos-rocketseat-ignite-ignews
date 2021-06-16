328a200b263798e24675f5374195baad4e9c8614
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-16 20:59:43 -0300

    Primeira execução de testes - com erro

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -4,9 +4,11 @@ import { render } from '@testing-library/react';
 import { ActiveLink } from '.';
 
 test('ActiveLink render correctly', () => {
-  render(
+  const { debug } = render(
     <ActiveLink href="/" activeClassName="active">
       <a>Home</a>
     </ActiveLink>
   )
+
+  debug();
 })
