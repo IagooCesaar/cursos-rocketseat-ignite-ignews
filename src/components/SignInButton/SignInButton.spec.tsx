e2f1d0f7e6a0a5667a8d754da0d471262fc3ed42
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-16 21:56:27 -0300

    definindo retorno específico de useSessionMocked em cada teste do SignInButton

--- src/components/SignInButton/SignInButton.spec.tsx
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -10,7 +10,7 @@ jest.mock('next-auth/client');
 describe('SignInButton component', () => {
   it('renders correctly when user is not authenticated', () => {
     const useSessionMocked = mocked(useSession);
-    useSessionMocked.mockReturnValue([null, false])
+    useSessionMocked.mockReturnValueOnce([null, false])
 
     render(
       <SignInButton />
@@ -19,6 +19,12 @@ describe('SignInButton component', () => {
   })
 
   it('renders correctly when user is authenticated', () => {
+    const useSessionMocked = mocked(useSession);
+    useSessionMocked.mockReturnValueOnce([
+      { user: { name: 'John Doe', email: 'john.doe@example.com' }, expires: 'fake-expires' },
+      false
+    ])
+
     render(
       <SignInButton />
     )
