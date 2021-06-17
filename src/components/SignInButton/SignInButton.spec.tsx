cae32894e2edb6eec1ab2f3b2770c4511d187fd1
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-16 21:50:55 -0300

    imitações customizadas com mocked do ts-jest

--- src/components/SignInButton/SignInButton.spec.tsx
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { mocked } from 'ts-jest/utils';
+import { useSession } from 'next-auth/client';
 
 import { SignInButton } from '.';
 
-jest.mock('next-auth/client', () => {
-  return {
-    useSession() {
-      return [null, false];
-    }
-  }
-})
+jest.mock('next-auth/client');
 
 describe('SignInButton component', () => {
   it('renders correctly when user is not authenticated', () => {
+    const useSessionMocked = mocked(useSession);
+
     render(
       <SignInButton />
     )
