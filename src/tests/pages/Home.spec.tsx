bf0fc57df5a4ff7f6c3b602cae74c4ec6ab469c9
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-17 20:01:55 -0300

    primeiro teste com erro, elemento não encontrado

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -4,6 +4,13 @@ import Home from '../../pages';
 describe("Home page", () => {
 
   it("Render correctly", () => {
+    render(<Home
+      product={{
+        priceId: 'fake-price-id',
+        amount: 'R$10,00',
+      }}
+    />)
 
+    expect(screen.getByText('R$10,00')).toBeInTheDocument();
   })
 })
