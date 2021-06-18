a210fc8701465d5f4a1fe60f274692e4144cde86
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-18 09:21:06 -0300

    teste getStaticProps para Posts page

--- src/tests/pages/Posts.spec.tsx
+++ src/tests/pages/Posts.spec.tsx
@@ -1,9 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import { mocked } from 'ts-jest/utils';
-import { stripe } from '../../services/stripe';
+import { getPrismicClient } from '../../services/prismic';
 
 import Posts, { getStaticProps } from '../../pages/posts';
 import React from 'react';
+import { isJsxExpression } from 'typescript';
+
+jest.mock('../../services/prismic');
 
 const posts = [
   { slug: 'my-new-post', title: 'My New Post', excerpt: 'Post excerpt', updatedAt: '10 de março' },
@@ -19,17 +22,39 @@ describe("Posts page", () => {
     expect(screen.getByText('My New Post')).toBeInTheDocument();
   })
 
-  // it('loads initial data', async () => {
-  //   const retrieveStripePriveMocked = mocked(stripe.prices.retrieve);
-  //   retrieveStripePriveMocked.mockResolvedValueOnce({
-  //     id: 'fake-id',
-  //     unit_amount: 1000,
-  //   } as any);
-  //   const response = await getStaticProps({});
-  //   expect(response).toEqual(
-  //     expect.objectContaining({
-  //       props: { product: { priceId: 'fake-id', amount: '$10,00' } },
-  //     })
-  //   )
-  // })
+  it('loads initial data', async () => {
+    const getPrismicClientMocked = mocked(getPrismicClient);
+
+    getPrismicClientMocked.mockReturnValueOnce({
+      query: jest.fn().mockResolvedValueOnce({
+        results: [
+          {
+            uid: 'my-new-post',
+            data: {
+              title: [
+                { type: 'heading', text: 'My New Post' },
+              ],
+              content: [
+                { type: 'paragraph', text: 'Post excerpt' }
+              ],
+            },
+            last_publication_date: '04-01-2021',
+          }
+        ]
+      }),
+    } as any);
+    const response = await getStaticProps({});
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          posts: [{
+            slug: 'my-new-post',
+            title: 'My New Post',
+            excerpt: 'Post excerpt',
+            updatedAt: '01 de abril de 2021'
+          }]
+        },
+      })
+    )
+  })
 })
