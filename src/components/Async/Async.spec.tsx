c2ee4c21f4e7d20301ecab8755e2142c1dce2ff3
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-18 11:01:58 -0300

    Espernado timeout para validar teste (get para find)

--- src/components/Async/Async.spec.tsx
+++ src/components/Async/Async.spec.tsx
@@ -2,10 +2,11 @@ import { render, screen } from '@testing-library/react'
 import { Async } from '.';
 
 describe('Exemplo para componentes com comportamentos assincronos', () => {
-  it('it render correctly', () => {
+  it('it render correctly', async () => {
     render(<Async />)
     expect(screen.getByText('Hello World')).toBeInTheDocument();
-    //Vai dar erro pq não está visível no momento do teste
-    expect(screen.getByText('Botão')).toBeInTheDocument();
+
+    //Ajuste para esperar algo acontecer em tela
+    expect(await screen.findByText('Botão')).toBeInTheDocument();
   })
 })
