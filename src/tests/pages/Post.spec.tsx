936aec0e73e156ad5ca0f90bb080b698b454b703
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-18 10:05:59 -0300

    teste carregamento inicial com getServerSideProps

--- src/tests/pages/Post.spec.tsx
+++ src/tests/pages/Post.spec.tsx
@@ -7,6 +7,7 @@ import React from 'react';
 import { getSession } from 'next-auth/client';
 
 jest.mock('next-auth/client');
+jest.mock('../../services/prismic');
 
 const post = {
   slug: 'my-new-post',
@@ -42,6 +43,45 @@ describe("Post page", () => {
         redirect: expect.objectContaining({
           destination: '/',
         })
+      })
+    )
+  })
+
+  it('load initial data', async () => {
+    const getSessionMocked = mocked(getSession);
+    getSessionMocked.mockResolvedValueOnce({
+      activeSubscription: 'fake-active-subscription',
+    } as any);
+
+    const getPrismicClientMocked = mocked(getPrismicClient);
+    getPrismicClientMocked.mockReturnValueOnce({
+      getByUID: jest.fn().mockResolvedValueOnce({
+        data: {
+          title: [
+            { type: 'heading', text: 'My new post' }
+          ],
+          content: [
+            { type: 'paragraph', text: 'Post content' }
+          ]
+        },
+        last_publication_date: '04-01-2021',
+      })
+    } as any);
+
+    const response = await getServerSideProps({
+      params: { slug: 'my-new-post' }
+    } as any);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          post: {
+            slug: 'my-new-post',
+            title: 'My new post',
+            content: '<p>Post content</p>',
+            updatedAt: '01 de abril de 2021'
+          }
+        }
       })
     )
   })
