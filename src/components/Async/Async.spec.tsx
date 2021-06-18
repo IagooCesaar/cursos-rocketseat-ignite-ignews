5e15e735d4cb7d7bdaae0497ece2473e491aa2bf
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-18 10:59:23 -0300

    Teste falha pois timout não finalizou

--- src/components/Async/Async.spec.tsx
+++ src/components/Async/Async.spec.tsx
@@ -5,5 +5,7 @@ describe('Exemplo para componentes com comportamentos assincronos', () => {
   it('it render correctly', () => {
     render(<Async />)
     expect(screen.getByText('Hello World')).toBeInTheDocument();
+    //Vai dar erro pq não está visível no momento do teste
+    expect(screen.getByText('Botão')).toBeInTheDocument();
   })
 })
